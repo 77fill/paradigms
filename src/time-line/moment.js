c9ad217b1fd7dcc6519a0c0d2c4e475645514f62
Philipp Schmalz <77fill@googlemail.com>
2025-05-05 17:13:10 +0200

    Improve Moment & Add img
    Use summary & shortTile in Moment

--- src/time-line/moment.js
+++ src/time-line/moment.js
@@ -1,5 +1,8 @@
 export default class Moment {
-    constructor(date, summary) {
-        
+    constructor(date, shortTitle, summary, imageUrl) {
+        this.date = date
+        this.shortTitle = shortTitle
+        this.summary = summary
+        this.imageUrl = imageUrl
     }
 }
