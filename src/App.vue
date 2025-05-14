5fd65bea98ee200b70041d7fec03f9a6758dd362
Philipp Schmalz <77fill@googlemail.com>
2025-05-14 17:16:29 +0200

    Add basic App structure with two component husks

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+  import timelineData from "@/domain/timeline-data.js";
+  import Timeline from "@/components/Timeline.vue";
+  import TimelineItem from "@/components/TimelineItem.vue";
 </script>
 
 <template>
   <v-app>
     <v-main>
-
+      <timeline>
+        <timeline-item
+            v-for="incident in timelineData"
+            :data="incident" />
+      </timeline>
     </v-main>
   </v-app>
 </template>
