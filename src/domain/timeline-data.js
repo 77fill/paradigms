f87c0dfc154b7cb909d5410cff08c2f5095dd5a0
Philipp Schmalz <77fill@googlemail.com>
2025-05-15 22:05:00 +0200

    Add 'Simula' to domain timeline-data

--- src/domain/timeline-data.js
+++ src/domain/timeline-data.js
@@ -1,7 +1,19 @@
 import moment from "moment";
 import structuredProgrammingIconUrl from "@/assets/structured-programming-icon.svg"
+import simulaUrl from "@/assets/simula.svg"
 
 export default [
+    {
+        date: moment([1962]),
+        importance: 1,
+        tags: ["oop"],
+        iconUrl: simulaUrl,
+        title: "Simula: First OOP Language",
+        summary: "Designed to be a language used for 'simulations' as the name suggests," +
+            " Simula I can be considered a rough superset of ALGOL 60." +
+            " It is seen as the first object-oriented language." +
+            " Simula 67 was later developed as a general purpose language.",
+    },
     {
         date: moment([1968]),
         importance: 1,
